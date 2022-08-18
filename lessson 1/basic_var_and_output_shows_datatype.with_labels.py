60601e5cc00de00fae9b7cd88924382c65f6e2b6
sammyj <103463178+samtheman24@users.noreply.github.com>
2022-08-18 12:14:41 +1200

    Add files via upload

---
+++ lessson 1/basic_var_and_output_shows_datatype.with_labels.py
@@ -0,0 +1,15 @@
+# s jones
+# varables
+
+
+# declaration of variables
+name = "elephant"
+numlegs = 4
+height = 4.5
+hastail = True
+
+# output to screen
+print('the animal:',name)
+print("the number of legs:",numlegs)
+print("height in metres:",height)
+print("it has a tail:",hastail)
