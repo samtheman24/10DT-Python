69bd4f23e5dfd052298cd8e240437cf95bd3da3b
sammyj <103463178+samtheman24@users.noreply.github.com>
2022-08-24 10:20:05 +1200

    Add files via upload

---
+++ lessson 1/task about me.py
@@ -0,0 +1,16 @@
+# S jones
+# practise task - variables, print, data type,
+
+#Variable declaration
+Name = "Sam jones"
+height = 161
+Phone = "04 939 3050"
+address = "170 coutts street, Wellington, NZ"
+allergy = "No"
+
+
+print('My name:',Name)
+print("Height in centimetres:",height)
+print("My schools phone number:", Phone)
+print("My schools address:", address)
+print("Do I have any allergys?:", allergy)
