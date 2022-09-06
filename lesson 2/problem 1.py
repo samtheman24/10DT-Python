b2af3ea82e94d2fda70ab2c016db41e0d680b79b
sammyj <103463178+samtheman24@users.noreply.github.com>
2022-09-07 10:49:44 +1200

    Add files via upload

---
+++ lesson 2/problem 1.py
@@ -0,0 +1,11 @@
+# S R Jones
+# calculation of variables, framing of labels, data in context
+
+# problem 01
+# this program is disigned to track and total goals by the RC 3rd XI for each game playyed
+
+match1_st_pats = 7
+match2_well_college = 5
+match3_well_high = 10
+team_total = match1_st_pats + match2_well_college + match3_well_high
+print("total goals scored", team_total)
