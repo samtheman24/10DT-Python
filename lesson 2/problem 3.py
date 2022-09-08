e1f5d9528002723cd4825489ed6f500b98458acc
sammyj <103463178+samtheman24@users.noreply.github.com>
2022-09-08 12:01:41 +1200

    Add files via upload

---
+++ lesson 2/problem 3.py
@@ -0,0 +1,15 @@
+# S R Jones
+dieselprice = 1.34
+petrol95price = 2.04
+petrol91price = 1.99
+print("initial price:")
+print("diesel price $" + str(dieselprice))
+print("95 price $" + str(petrol95price))
+print("91 unleaded price" + str(petrol91price))
+dieselprice = dieselprice * 1.1
+petrol95price = petrol95price * 1.1
+petrol91price = petrol91price * 1.1
+print("price increase by 10%:")
+print("diesel price $" + str(dieselprice))
+print("95 price $ " + str(petrol95price))
+print("91 unleaded price $ " + str(petrol91price))
