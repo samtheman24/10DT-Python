900221bc5d9802bbdf8d7b46c3d8d4c231565686
sammyj <103463178+samtheman24@users.noreply.github.com>
2022-09-08 12:29:45 +1200

    Add files via upload

---
+++ lesson 2/variable print with dollar sign.py
@@ -0,0 +1,15 @@
+#S R Jones
+# end for concatenation in output
+
+dollars = 5
+print("$", dollars, ".00")
+
+dollars = 5
+print("$", end = '')
+print(dollars, end='')
+print(".00")
+
+winner = "waterboy"
+print("first place:", winner) # note the blank space between''
+print("first place:", end= ' ') # note the blank space between ''
+print(winner)
